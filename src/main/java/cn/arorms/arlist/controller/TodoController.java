50b841d513cdf276265bc8b4a47495ea83ac0bc3
Cacciatore <holmesamzish86@outlook.com>
2025-05-17 20:24:36 +0800

    Update hide completed task

--- src/main/java/cn/arorms/arlist/controller/TodoController.java
+++ src/main/java/cn/arorms/arlist/controller/TodoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/todos")
+@RequestMapping("/api/todos")
 public class TodoController {
 
     @Autowired
