c3daaca8a6a76728395a477ccb2a0cd4ea76e3f8
cacc <Holmesamzish86@outlook.com>
2026-01-31 23:17:04 +0800

    feat(ui) add todo modification

--- backend/src/main/java/cn/arorms/list/backend/controller/GroupController.java
+++ backend/src/main/java/cn/arorms/list/backend/controller/GroupController.java
@@ -5,7 +5,6 @@ import cn.arorms.list.backend.service.GroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController @RequestMapping("/api/group")
@@ -16,21 +15,25 @@ public class GroupController {
         this.groupService = groupService;
     }
 
+    // Get all groups
     @GetMapping()
     public List<Group> getAllGroups() {
         return groupService.getAllGroups();
     }
 
+    // Add a new group
     @PostMapping()
     public ResponseEntity<Group> addGroup(@RequestBody Group group) {
         return ResponseEntity.ok(groupService.addGroup(group));
     }
 
+    // Modify the group
     @PutMapping()
     public ResponseEntity<Group> updateGroup(@RequestBody Group group) {
         return ResponseEntity.ok(groupService.updateGroup(group));
     }
 
+    // Delete the group
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteGroup(@PathVariable Long id) {
         groupService.deleteGroup(id);
--- backend/src/main/java/cn/arorms/list/backend/repository/TodoRepository.java
+++ backend/src/main/java/cn/arorms/list/backend/repository/TodoRepository.java
@@ -1,8 +1,12 @@
 package cn.arorms.list.backend.repository;
 
+import cn.arorms.list.backend.pojo.entity.Group;
 import cn.arorms.list.backend.pojo.entity.Todo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+    Page<Todo> findByGroupId(Long groupId, Pageable sortedPageable);
     // Additional query methods can be defined here if needed
 }
